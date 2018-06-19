bec698959c534786987f6bd6bff196d737fc4341
Jason.Wu(吳志平) <wujpkimo@gmail.com>
2018-06-19 21:05:17 +0800

    2018-06-19 Jason Lession1

---
+++ Lession1/Lession1/DataType2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            float PI_f = 3.14159265359f;
+            double PI_d = 3.14159265359;
+            Console.WriteLine("float的PI：{0}", PI_f);
+            Console.WriteLine("double的PI：{0}", PI_d);
+        }
+    }
+}
---
+++ Lession1/Lession1/Exercise3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int number = 999999999999;
+            Console.WriteLine("輸入的整數是：{0}", number);
+        }
+    }
+}
